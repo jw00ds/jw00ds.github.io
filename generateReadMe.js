9f165dd17e03c004933b30079d1aa641b26e9c46
jw00ds <woods.johnc@yahoo.com>
2020-12-03 23:50:33 -0600

    feat: placeholder for potential md-html functionality

---
+++ generateReadMe.js
@@ -0,0 +1,7 @@
+// import marked from "marked";
+// import fs from "fs";
+
+// const readMe = fs.readFileSync("README.md", "utf-8");
+// const markdownReadMe = marked(readMe);
+
+// fs.writeFileSync("./site/README.html", markdownReadMe);
