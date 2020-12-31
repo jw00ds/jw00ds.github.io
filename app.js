7eaf05d45830e035264e73a5a03abb1304449804
jw00ds <woods.johnc@yahoo.com>
2020-12-31 11:43:48 -0600

    refactor: add return statement and join elements of mapped innerHTML array

--- app.js
+++ app.js
@@ -18,39 +18,40 @@ window.addEventListener('DOMContentLoaded',
     async () => {
         const readings = await fetchReadings();
         const latestFiveReadings = readings.slice(0, 5);
-        document.getElementById('breads').innerHTML = latestFiveReadings.map(reading => {
-            `
-        <main class="container">
-            <section class="row justify-content-center mt-3">
-                <div class="col-12">
-                    <div class="card shadow-lg">
-                        <div class="card-body">
-                            <h2 class="card-title">${reading.title}</h2>
-                            <h3 class="card-subtitle mb-2 text-muted">
-                                Timestamp: ${reading.created_at}
-                            </h3>
-                            <img
-                                src=${reading.reading_image}
-                                alt="Article img"
-                                class="thumbnail"
-                            />
-                            <p class="card-text">
-                                ${reading.description.substring(0, 125)}
-                            </p>
-                            <div class="d-flex justify-content-end">
-                                <a
-                                    href=${reading.url}
-                                    class="btn btn-link"
-                                >
-                                    Link to full article
-                                </a>
+        let breadsHtml = latestFiveReadings.map(reading => {
+            return `
+                <main class="container">
+                    <section class="row justify-content-center mt-3">
+                        <div class="col-12">
+                            <div class="card shadow-lg">
+                                <div class="card-body">
+                                    <h2 class="card-title">${reading.title}</h2>
+                                    <h3 class="card-subtitle mb-2 text-muted">
+                                        Timestamp: ${reading.created_at}
+                                    </h3>
+                                    <img
+                                        src=${reading.reading_image}
+                                        alt="Article img"
+                                        class="thumbnail"
+                                    />
+                                    <p class="card-text">
+                                        ${reading.description.substring(0, 125)}
+                                    </p>
+                                    <div class="d-flex justify-content-end">
+                                        <a
+                                            href=${reading.url}
+                                            class="btn btn-link"
+                                        >
+                                            Link to full article
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </section>
-        </main>
-        `;
-        });
+                    </section>
+                </main>
+            `;
+        }).join('');
+        document.getElementById('breads').innerHTML = breadsHtml;
     }
 );
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -41,7 +41,7 @@ function fetchReadings() {
 }
 
 window.addEventListener('DOMContentLoaded', function _callee() {
-  var readings, latestFiveReadings;
+  var readings, latestFiveReadings, breadsHtml;
   return regeneratorRuntime.async(function _callee$(_context2) {
     while (1) {
       switch (_context2.prev = _context2.next) {
@@ -52,11 +52,12 @@ window.addEventListener('DOMContentLoaded', function _callee() {
         case 2:
           readings = _context2.sent;
           latestFiveReadings = readings.slice(0, 5);
-          document.getElementById('breads').innerHTML = latestFiveReadings.map(function (reading) {
-            "\n        <main class=\"container\">\n            <section class=\"row justify-content-center mt-3\">\n                <div class=\"col-12\">\n                    <div class=\"card shadow-lg\">\n                        <div class=\"card-body\">\n                            <h2 class=\"card-title\">".concat(reading.title, "</h2>\n                            <h3 class=\"card-subtitle mb-2 text-muted\">\n                                Timestamp: ").concat(reading.created_at, "\n                            </h3>\n                            <img\n                                src=").concat(reading.reading_image, "\n                                alt=\"Article img\"\n                                class=\"thumbnail\"\n                            />\n                            <p class=\"card-text\">\n                                ").concat(reading.description.substring(0, 125), "\n                            </p>\n                            <div class=\"d-flex justify-content-end\">\n                                <a\n                                    href=").concat(reading.url, "\n                                    class=\"btn btn-link\"\n                                >\n                                    Link to full article\n                                </a>\n                            </div>\n                        </div>\n                    </div>\n                </div>\n            </section>\n        </main>\n        ");
-          });
+          breadsHtml = latestFiveReadings.map(function (reading) {
+            return "\n                <main class=\"container\">\n                    <section class=\"row justify-content-center mt-3\">\n                        <div class=\"col-12\">\n                            <div class=\"card shadow-lg\">\n                                <div class=\"card-body\">\n                                    <h2 class=\"card-title\">".concat(reading.title, "</h2>\n                                    <h3 class=\"card-subtitle mb-2 text-muted\">\n                                        Timestamp: ").concat(reading.created_at, "\n                                    </h3>\n                                    <img\n                                        src=").concat(reading.reading_image, "\n                                        alt=\"Article img\"\n                                        class=\"thumbnail\"\n                                    />\n                                    <p class=\"card-text\">\n                                        ").concat(reading.description.substring(0, 125), "\n                                    </p>\n                                    <div class=\"d-flex justify-content-end\">\n                                        <a\n                                            href=").concat(reading.url, "\n                                            class=\"btn btn-link\"\n                                        >\n                                            Link to full article\n                                        </a>\n                                    </div>\n                                </div>\n                            </div>\n                        </div>\n                    </section>\n                </main>\n            ");
+          }).join('');
+          document.getElementById('breads').innerHTML = breadsHtml;
 
-        case 5:
+        case 6:
         case "end":
           return _context2.stop();
       }
