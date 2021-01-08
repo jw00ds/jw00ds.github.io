081e2016c76a8f46117d84035084679d6bf6b7e4
jw00ds <woods.johnc@yahoo.com>
2021-01-08 15:29:48 -0600

    refactor: use native JS date formatting

--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,9 +1,11 @@
 "use strict";
 
-var _moment = _interopRequireDefault(require("moment"));
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.eventListener = void 0;
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
+// import moment from 'moment';
 function fetchReadings() {
   var response, responseBody;
   return regeneratorRuntime.async(function fetchReadings$(_context) {
@@ -44,27 +46,41 @@ function fetchReadings() {
   }, null, null, [[0, 10]]);
 }
 
-window.addEventListener('DOMContentLoaded', function _callee() {
-  var readings, latestFiveReadings, breadsHtml;
-  return regeneratorRuntime.async(function _callee$(_context2) {
-    while (1) {
-      switch (_context2.prev = _context2.next) {
-        case 0:
-          _context2.next = 2;
-          return regeneratorRuntime.awrap(fetchReadings());
+var eventListener = function eventListener() {
+  return window.addEventListener('DOMContentLoaded', function _callee() {
+    var readings, latestFiveReadings, breadsHtml;
+    return regeneratorRuntime.async(function _callee$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _context2.next = 2;
+            return regeneratorRuntime.awrap(fetchReadings());
 
-        case 2:
-          readings = _context2.sent;
-          latestFiveReadings = readings.slice(0, 5);
-          breadsHtml = latestFiveReadings.map(function (reading) {
-            return "\n                <main class=\"container\">\n                    <section class=\"row justify-content-center mt-3\">\n                        <div class=\"col-12\">\n                            <div id=\"breads-cards\" class=\"card shadow-lg\">\n                                <div id=\"breads-cards\" class=\"card-body\">\n                                    <h2 id=\"breads-cards\" class=\"card-title\">".concat(reading.title, "</h2>\n                                    <h3 id=\"breads-cards\" class=\"card-subtitle mb-2 text-muted\">\n                                        Timestamp: ").concat((0, _moment["default"])(reading.created_at), "\n                                    </h3>\n                                    <img\n                                        src=").concat(reading.reading_image, "\n                                        alt=\"Article img\"\n                                        class=\"thumbnail\"\n                                    />\n                                    <p id=\"breads-cards\" class=\"card-text\">\n                                        ").concat(reading.description.substring(0, 300), "...\n                                    </p>\n                                    <div id=\"breads-cards\" class=\"d-flex justify-content-end\">\n                                        <a\n                                            id=\"breads-cards\"\n                                            href=").concat(reading.url, "\n                                            class=\"btn btn-link\"\n                                        >\n                                            Link to full article\n                                        </a>\n                                    </div>\n                                </div>\n                            </div>\n                        </div>\n                    </section>\n                </main>\n            ");
-          }).join('');
-          document.getElementById('breads').innerHTML = breadsHtml;
+          case 2:
+            readings = _context2.sent;
+            latestFiveReadings = readings.slice(0, 5);
+            breadsHtml = latestFiveReadings.map(function (reading) {
+              var d = new Date(reading.created_at);
+              var yr = new Intl.DateTimeFormat('en', {
+                year: 'numeric'
+              }).format(d);
+              var mo = new Intl.DateTimeFormat('en', {
+                month: 'short'
+              }).format(d);
+              var day = new Intl.DateTimeFormat('en', {
+                day: '2-digit'
+              }).format(d);
+              return "\n                <main class=\"container\">\n                    <section class=\"row justify-content-center mt-3\">\n                        <div class=\"col-12\">\n                            <div id=\"breads-cards\" class=\"card shadow-lg\">\n                                <div id=\"breads-cards\" class=\"card-body\">\n                                    <h2 id=\"breads-cards\" class=\"card-title\">".concat(reading.title, "</h2>\n                                    <h3 id=\"breads-cards\" class=\"card-subtitle mb-2 text-muted\">\n                                        Timestamp: ").concat(day, "-").concat(mo, "-").concat(yr, "\n                                    </h3>\n                                    <img\n                                        src=").concat(reading.reading_image, "\n                                        alt=\"Article img\"\n                                        class=\"thumbnail\"\n                                    />\n                                    <p id=\"breads-cards\" class=\"card-text\">\n                                        ").concat(reading.description.substring(0, 300), "...\n                                    </p>\n                                    <div id=\"breads-cards\" class=\"d-flex justify-content-end\">\n                                        <a\n                                            id=\"breads-cards\"\n                                            href=").concat(reading.url, "\n                                            class=\"btn btn-link\"\n                                        >\n                                            Link to full article\n                                        </a>\n                                    </div>\n                                </div>\n                            </div>\n                        </div>\n                    </section>\n                </main>\n            ");
+            }).join('');
+            document.getElementById('breads').innerHTML = breadsHtml;
 
-        case 6:
-        case "end":
-          return _context2.stop();
+          case 6:
+          case "end":
+            return _context2.stop();
+        }
       }
-    }
-  });
-});
+    });
+  })();
+};
+
+exports.eventListener = eventListener;
